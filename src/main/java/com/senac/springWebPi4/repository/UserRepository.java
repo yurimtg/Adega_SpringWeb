50339055b078c9cf6351391b5fbcc5101f1cef7d
Yuri de Oliveira Ignácio <y.oliveira.ignacio@avanade.com>
2022-03-12 10:48:40 -0300

    Busca Dados

--- src/main/java/com/senac/springWebPi4/repository/UserRepository.java
+++ src/main/java/com/senac/springWebPi4/repository/UserRepository.java
@@ -2,8 +2,16 @@
 package com.senac.springWebPi4.repository;
 
 import com.senac.springWebPi4.model.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long>{
+    
+ @Query("select u from User u where u.id > :id")   
+ public List<User> findAllMoreThan(@Param("id") Long id);
+ public List<User> findByIdGreaterThan(Long id);
+ public List<User> findByNomeIgnoreCase(String id);
     
 }
