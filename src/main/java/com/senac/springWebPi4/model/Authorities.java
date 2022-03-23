9ab4ff4d4b105c51eae9f8583a41dc2da8493560
DanielSanoli <daniel.sanoli28@gmail.com>
2022-03-23 19:29:25 -0300

    Adicionando Getters e Setters nas entidades

--- src/main/java/com/senac/springWebPi4/model/Authorities.java
+++ src/main/java/com/senac/springWebPi4/model/Authorities.java
@@ -1,18 +1,48 @@
 package com.senac.springWebPi4.model;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import javax.persistence.*;
 
-@Getter
-@Setter
 @Entity
 @Table(name = "authorities")
 public class Authorities {
-    @Id
+   
+	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String username;
     private String authority;
+    
+    public Authorities() {
+	}
+
+	public Authorities(String username, String authority) {
+		this.username = username;
+		this.authority = authority;
+	}
+
+	public long getId() {
+		return id;
+	}
+    
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getAuthority() {
+		return authority;
+	}
+	
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+    
+    
 }
--- src/main/java/com/senac/springWebPi4/model/User.java
+++ src/main/java/com/senac/springWebPi4/model/User.java
@@ -1,17 +1,11 @@
 package com.senac.springWebPi4.model;
 
-import lombok.*;
-
 import javax.persistence.*;
 
-@Getter
-@Setter
-@ToString
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 @Table(name = "users")
 public class User {
+	
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -19,6 +13,73 @@ public class User {
     private String username;
     private String password;
     private Boolean enabled;
+    
     @Enumerated(EnumType.STRING)
     private TipoUsuario tipoUsuario;
+    
+    public User() {
+	}
+
+	public User(String name, String username, String password, Boolean enabled, TipoUsuario tipoUsuario) {
+		this.name = name;
+		this.username = username;
+		this.password = password;
+		this.enabled = enabled;
+		this.tipoUsuario = tipoUsuario;
+	}
+
+	public long getId() {
+		return id;
+	}
+    
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public Boolean getEnabled() {
+		return enabled;
+	}
+	
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+	
+	public TipoUsuario getTipoUsuario() {
+		return tipoUsuario;
+	}
+	
+	public void setTipoUsuario(TipoUsuario tipoUsuario) {
+		this.tipoUsuario = tipoUsuario;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", username=" + username + ", password=" + password + ", enabled="
+				+ enabled + ", tipoUsuario=" + tipoUsuario + "]";
+	}
+	
 }
