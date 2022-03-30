e60e135249eb6feb8b2e04afdf182a5487ee4dfc
y.oliveira.ignacio <y.oliveira.ignacio@WINAPRmAEP0D4cP.corp.avanade.org>
2022-03-29 21:51:07 -0300

    paginação, imagem

--- src/main/java/com/senac/springWebPi4/model/Produto.java
+++ src/main/java/com/senac/springWebPi4/model/Produto.java
@@ -1,9 +1,11 @@
 package com.senac.springWebPi4.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 
 @Entity
 public class Produto {
@@ -14,61 +16,80 @@ public class Produto {
     private String nomeProduto;
     private long qtd;
     private String status;
+    @Column(length = 1000)
     private String descricao;
-    
+    private String imagem;
+    private double valor;
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
+
     public Produto() {
-	}
+    }
 
-	public Produto(String nomeProduto, long qtd, String status, String descricao) {
-		this.nomeProduto = nomeProduto;
-		this.qtd = qtd;
-		this.status = status;
-		this.descricao = descricao;
-	}
+    public Produto(String nomeProduto, long qtd, String status, String descricao) {
+        this.nomeProduto = nomeProduto;
+        this.qtd = qtd;
+        this.status = status;
+        this.descricao = descricao;
+    }
 
-	public long getId() {
-		return id;
-	}
+    public long getId() {
+        return id;
+    }
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    public void setId(long id) {
+        this.id = id;
+    }
 
-	public String getNomeProduto() {
-		return nomeProduto;
-	}
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
 
-	public void setNomeProduto(String nomeProduto) {
-		this.nomeProduto = nomeProduto;
-	}
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
 
-	public long getQtd() {
-		return qtd;
-	}
+    public long getQtd() {
+        return qtd;
+    }
 
-	public void setQtd(long qtd) {
-		this.qtd = qtd;
-	}
+    public void setQtd(long qtd) {
+        this.qtd = qtd;
+    }
 
-	public String getStatus() {
-		return status;
-	}
+    public String getStatus() {
+        return status;
+    }
 
-	public void setStatus(String status) {
-		this.status = status;
-	}
+    public void setStatus(String status) {
+        this.status = status;
+    }
 
-	public String getDescricao() {
-		return descricao;
-	}
+    public String getDescricao() {
+        return descricao;
+    }
 
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 
-	@Override
+    @Override
     public String toString() {
         return "Produto{" + "id=" + id + ", nomeProduto=" + nomeProduto + ", qtd=" + qtd + ", status=" + status + ", descricao=" + descricao + '}';
     }
-      
+
 }
--- src/main/java/com/senac/springWebPi4/repository/ProdutoRepository.java
+++ src/main/java/com/senac/springWebPi4/repository/ProdutoRepository.java
@@ -1,14 +1,16 @@
 package com.senac.springWebPi4.repository;
 
 import com.senac.springWebPi4.model.Produto;
-import java.util.List;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.domain.Pageable;
 
 public interface ProdutoRepository extends PagingAndSortingRepository<Produto, Long> {
+    
+    @Query(value = "SELECT * FROM PRODUTO WHERE nome_produto like %?1%", nativeQuery = true)
 
-//    @Query("select p from Produto p order by p desc")
+//    @Query("select p from produto p where p.nomeProduto like :nome%")
+    Page<Produto> findByNomeProduto(String nome, Pageable page);
 
-//    @Query("select p from produto p where p.nome like ?1%")
-//    List<Produto> findByName(String nome);
 }
