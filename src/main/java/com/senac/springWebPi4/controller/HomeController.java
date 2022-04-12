6ec3885c173527722a8396106986ac2946ad016a
y.oliveira.ignacio <y.oliveira.ignacio@WINAPRmAEP0D4cP.corp.avanade.org>
2022-04-12 06:26:55 -0300

    home page imagens

--- src/main/java/com/senac/springWebPi4/controller/HomeController.java
+++ src/main/java/com/senac/springWebPi4/controller/HomeController.java
@@ -4,6 +4,7 @@ import com.senac.springWebPi4.model.Imagem;
 import com.senac.springWebPi4.model.Produto;
 import com.senac.springWebPi4.repository.ImagemRepository;
 import com.senac.springWebPi4.repository.ProdutoRepository;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,8 +27,45 @@ public class HomeController {
     @GetMapping("/home")
     public ModelAndView homePage() {
         ModelAndView mv = new ModelAndView("/home");
-        List <Produto> prod =  (List <Produto>) produtoRepository.findAll();
+        List<Produto> prod = produtoRepository.findAll();
+        List<Imagem> img = imagemRepository.findAll();
+        List<Imagem> firstImg = new ArrayList();
+        List<Imagem> imagens = new ArrayList();
+
+        for (Produto produto : prod) {
+            boolean achou = false;
+            for (Imagem imagem : img) {
+                if (produto.getId() == imagem.getFk_prodId() && achou == false) {
+                    Imagem fImg = new Imagem();
+                    fImg.setImg(imagem.getImg());
+                    fImg.setFk_prodId(imagem.getFk_prodId());
+                    fImg.setId(imagem.getId());
+                    firstImg.add(fImg);
+                    achou = true;
+                }
+            }
+        }
+
+        for (Imagem imagem : img) {
+            boolean achou = false;
+            for (Imagem fImg : firstImg) {
+                if (imagem.getId() == fImg.getId()) {
+                    achou = true;
+                }
+            }
+            if (achou == false) {
+                Imagem imgAux = new Imagem();
+                imgAux.setImg(imagem.getImg());
+                imgAux.setFk_prodId(imagem.getFk_prodId());
+                imgAux.setId(imagem.getId());
+                imagens.add(imgAux);
+            }
+        }
+
         mv.addObject("produto", prod);
+        mv.addObject("imagem", img);
+        mv.addObject("fistImg", firstImg);
+        mv.addObject("outrasImg", imagens);
 
         return mv;
     }
--- src/main/java/com/senac/springWebPi4/repository/ProdutoRepository.java
+++ src/main/java/com/senac/springWebPi4/repository/ProdutoRepository.java
@@ -1,6 +1,7 @@
 package com.senac.springWebPi4.repository;
 
 import com.senac.springWebPi4.model.Produto;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -12,5 +13,6 @@ public interface ProdutoRepository extends PagingAndSortingRepository<Produto, L
 
 //    @Query("select p from produto p where p.nomeProduto like :nome%")
     Page<Produto> findByNomeProduto(String nome, Pageable page);
+    List<Produto> findAll();
 
 }
